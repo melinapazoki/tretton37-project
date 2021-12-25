fa72d2eea4a71cf1c29541665c8ed056db4a4dee
Melina <melina.pazoki@tecnotree.com>
2021-12-25 16:33:10 +0330

    project setup + config file + package.json

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import "./App.css";
+
+const App = () => {
+  };
+  return (
+    <Grid container spacing={4}>
+      <Grid item xs={12}>
+        search bar part
+      </Grid>
+
+      <Grid item xs={12}>
+        search result bar
+      </Grid>
+    </Grid>
+  );
+};
+
+export default App;
---
+++ src/config.js
@@ -0,0 +1,6 @@
+const Config = {
+  APIKey:
+    "api-key 14:2021-12-16:lucas.stenberg@tretton37.com 54447cf099066c8f17404cb936f22c001f82bce1d630b8f9fb8b897fba7c8505",
+  url: "https://api.1337co.de/v3/employees",
+};
+export default Config;
