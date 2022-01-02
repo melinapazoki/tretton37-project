b62a1a7269b83ea12a23e6bb573dc5cd8e6ef319
Melina <melina.pazoki@tecnotree.com>
2022-01-02 14:39:33 +0330

    docker file and configuation api

--- src/services/api.js
+++ src/services/api.js
@@ -1,35 +1,37 @@
-export const handleError = async () => {};
-export const fetchResponse = async (url, Method) => {
-  return await fetch(entityURL, {
-    method: "GET",
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      Authorization:
-        "api-key 14:2021-12-16:lucas.stenberg@tretton37.com 54447cf099066c8f17404cb936f22c001f82bce1d630b8f9fb8b897fba7c8505",
-    },
-  })
-    .then(async (result) => {
-      const response = await result.json().catch(() => {});
-      return response;
-    })
-    .catch(() => {
-      return null;
-    });
+import Config from '../config'
+
+export const handleError = async () => {
 };
-export default getQuery = (entityURL, start, end, filter) => {
-  if (start) entityURL += `?start=${start}`;
-  if (end) entityURL += `&end=${end}`;
-  if (filter) entityURL += `?filter=${filter}`;
-  return entityURL;
+export const fetchResponse = async (url, Method) => {
+    return await fetch(url, {
+        method: Method,
+        headers: {
+            //"Access-Control-Allow-Origin": "*",
+            Authorization: Config.api_key,
+        },
+    })
+        .then(async (result) => {
+            const response = await result.json().catch(() => {
+            });
+            return response;
+        })
+        .catch(() => {
+            return null;
+        });
+};
+export const getQuery = (entityURL, start, end, filter) => {
+    if (start) entityURL += `?start=${start}`;
+    if (end) entityURL += `&end=${end}`;
+    if (filter) entityURL += `?filter=${filter}`;
+    return entityURL;
 };
 export const fetchCoworkerList = async (start, end, filter) => {
-  const entityURL = "//api.1337co.de/v3/employees";
-  const url = getQuery(entityURL, start, end, filter);
-  const Method = "GET";
+    const url = getQuery(Config.api_url, start, end, filter);
+    const Method = "GET";
 
-  try {
-    return await fetchResponse(url, Method);
-  } catch (e) {
-    return e;
-  }
+    try {
+        return await fetchResponse(url, Method);
+    } catch (e) {
+        return e;
+    }
 };
