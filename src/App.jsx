a986eeb0c6231fb47a94de6a9bdb36b855bd157b
Melina <melina.pazoki@tecnotree.com>
2022-01-12 02:25:28 +0330

    change app search

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,90 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Grid from "@mui/material/Grid";
 import "./App.css";
 import { fetchCoworkerList } from "./services/api";
 import SearchBar from "./components/SearchBar";
 import SearchResultList from "./components/SearchResultList";
 import AlphabetFilter from "./components/AlphabetFilter";
+import Path from "./components/Path";
+import DataNotAvailable from "./components/DataNotAvailable";
+
 const App = () => {
-  useEffect(() => {
-    getCoworkerList();
-  }, []);
+  // statefull component - main one - other components are stateless
   const [coworkers, setList] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
   const [officeList, setOfficeList] = useState([]);
+  const [originalValue, setOriginalValue] = useState([]);
   const [gridView, setGridView] = useState(true);
   const [alphabetFilter, setShowAlphabetFilter] = useState(false);
   const [personSearch, setsearchValue] = useState(true);
-  const getCoworkerList = async () => {
-    const coworkers = await fetchCoworkerList();
-    let officeList = [];
-    coworkers.map((item) => {
-      return !officeList.includes(item.office)
-        ? officeList.push(item.office)
-        : null;
-    });
-    setList(coworkers);
-    setOfficeList(officeList);
-  };
+  const [showLoader, setShowLoader] = useState(false);
+
+  useEffect(() => {
+    async function getCoworkerList() {
+      setShowLoader(true);
+      let coworkers;
+      // to avoid call api rapidlly
+      if (originalValue.length > 0) {
+        coworkers = originalValue;
+      } else {
+        coworkers = await fetchCoworkerList();
+        setOriginalValue(coworkers);
+        getOfficeList(coworkers);
+      }
+      const results = searchTerm
+        ? coworkers.filter((i) => {
+            return i.name.trim().toLowerCase().includes(searchTerm);
+          })
+        : coworkers;
+      setList(results);
+      setShowLoader(false);
+    }
+    function getOfficeList() {
+      let officeList = [];
+      originalValue.map((item) => {
+        return !officeList.includes(item.city)
+          ? // ? officeList.push(item.office)
+            officeList.push(item.city)
+          : null;
+      });
+      setOfficeList(officeList);
+    }
+    getCoworkerList();
+  }, [searchTerm, originalValue]);
 
   const onSearch = async (e) => {
-    const query = e.target.value.trim().toLowerCase();
-    const searchResult = coworkers.filter((item) => {
-      return item.name.trim().toLowerCase().indexOf(query) > -1;
-    });
-    // fetch query from backend
+    setSearchTerm(e.target.value);
+    // note: Backend search - if its enable
     // const coworkers = await fetchCoworkerList(null, null, query);
+    // setList(coworkers);
+  };
+  const onAlphabetSearch = async (e) => {
+    const query = e.target.innerText.toLowerCase();
+    // note: Backend search - if its enable
+    // const coworkers = await fetchCoworkerList(null, null, query);
+    debugger;
+    const filteredList =
+      query === "all"
+        ? originalValue
+        : originalValue.filter((item) =>
+            item.name.toLowerCase().startsWith(query)
+          );
+    setList(filteredList);
+  };
+  const handlesearchOffice = (e) => {
+    const searchResult = originalValue.filter((item) => {
+      return item.city.toLowerCase().indexOf(e.target.value.toLowerCase()) > -1;
+    });
     setList(searchResult);
   };
+
   const changeSearchValue = () => {
     setsearchValue(!personSearch);
   };
   const changeGridView = () => {
     setGridView(!gridView);
   };
-  const onAlphabetSearch = async (e) => {
-    const query = e.target.innerHTML;
-    const coworkers = await fetchCoworkerList(null, null, query);
-    coworkers.filter((item) =>
-      item.name.toLowerCase().startsWith(e.target.innerHTML.toLowerCase())
-    );
-    setList(coworkers);
-  };
-  const handlesearchOffice = (e) => {
-    const searchResult = coworkers.filter((item) => {
-      return (
-        item.office.toLowerCase().indexOf(e.target.value.toLowerCase()) > -1
-      );
-    });
-    setList(searchResult);
-  };
+
   const showFilter = () => {
     setShowAlphabetFilter(!alphabetFilter);
   };
@@ -64,6 +92,7 @@ const App = () => {
     <Grid container spacing={4}>
       <Grid item xs={12} className="searchBar">
         <SearchBar
+          searchTerm={searchTerm}
           onSearch={onSearch}
           changeGridView={changeGridView}
           gridView={gridView}
@@ -74,14 +103,23 @@ const App = () => {
           showFilter={showFilter}
         />
       </Grid>
-      {alphabetFilter && (
-        <Grid item xs={12}>
-          <AlphabetFilter onAlphabetSearch={onAlphabetSearch} />
-        </Grid>
+      {showLoader ? (
+        <DataNotAvailable loader={showLoader} />
+      ) : (
+        <>
+          {alphabetFilter && (
+            <Grid item xs={12}>
+              <AlphabetFilter onAlphabetSearch={onAlphabetSearch} />
+            </Grid>
+          )}
+          <Grid item xs={12}>
+            <Path mainPath={"Meet Employees"} subPath={searchTerm} />
+          </Grid>
+          <Grid item xs={12}>
+            <SearchResultList coworkers={coworkers} gridView={gridView} />
+          </Grid>
+        </>
       )}
-      <Grid item xs={12}>
-        <SearchResultList coworkers={coworkers} gridView={gridView} />
-      </Grid>
     </Grid>
   );
 };
